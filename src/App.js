380e0b85043dd2d9280bed1affdd09b2449aff6d
G.Mustafa Shaikh <34856975+TheMustafaShaikh@users.noreply.github.com>
2020-07-14 14:05:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [input,setInput] = useState("");
+  let [main,setMain] = useState("");
+  let [temp,setTemp] = useState("");
+  let [img,setImg] = useState("");
+  
+
+  let url = "https://samples.openweathermap.org/data/2.5/forecast?q=";
+  let apiKey ="&mode=JSON&appid=439d4b804bc8187953eb36d2a8c26a02";
+
+  const showData = ()=>{
+    const countryName = input;
+    let url = "https://api.openweathermap.org/data/2.5/weather?q="+countryName+"&appid=279b4be6d54c8bf6ea9b12275a567156";
+  
+    fetch(url).then((Response)=>{
+        let data = Response.json();
+        return data;
+      }).then((getdata)=>{
+        setImg("/images/"+getdata.weather[0].icon + ".png");
+        setMain(getdata.weather[0].main);
+        setTemp(getdata.main.temp+"F");
+      }).catch(()=>alert("Please enter correct country!"))
+ 
+      
+  }
+
+     return ( 
+      <div className = "App">
+        <h1 className="head-name">Weather App</h1>
+        <input value={input} onChange={(e)=>setInput(e.target.value)} id="search-input"/>
+        <button disabled={!input} onClick={showData} id="Searchbtn">Search</button>
+
+        <div className="info">
+          <img src={img}/>
+          {console.log(img)}
+         <h3><strong>{main}</strong></h3>
+          <h5><strong>{temp}</strong></h5>
+
+        </div>
+
+       </div>
+    );
 }
 
 export default App;
